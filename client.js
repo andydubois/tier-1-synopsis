3508bb3b073a197051aa4fc1e0a1ea4671ff8c19
Andy DuBois <andydubois89@gmail.com>
2019-07-17 13:13:15 -0500

    added ability to purchase items and add price, which will get pushed into an array

--- client.js
+++ client.js
@@ -1,3 +1,31 @@
-function readyNow() {
-    console.log('JQ');
+//runs the ready now function when the document is ready
+$( document ).ready( readyNow );
+
+const budget = 25000;
+let purchases = [];
+
+function addPurchase(){
+    console.log('in addPurchase');
+    //get user input and create new object
+    let newPurchase = {
+        name: $('#purchaseNameIn').val(),
+        price: $('#purchasePriceIn').val(),
+
+    }
+    //push new purchase into the array
+    purchases.push(newPurchase);
+    //empty inputs
+    $('#purchaseNameIn').val('');
+    $('#purchasePriceIn').val('');
+    //calculate remainingBudget
 }
+
+function readyNow() {
+    //display budget
+    //target budgetOut by id
+let el = $('#budgetOut');
+el.empty();
+el.append( budget );
+//handle click event
+$('#addPurchaseButton').on('click', addPurchase);
+} //end readyNow
