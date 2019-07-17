890f3e6914f7fbc0ca83c0d0494664c56194d965
Andy DuBois <andydubois89@gmail.com>
2019-07-17 14:07:30 -0500

    finished off practice exercie, can add item and price and it will be displayed on the screen in an unorderered list

--- client.js
+++ client.js
@@ -16,9 +16,42 @@ function addPurchase(){
     purchases.push(newPurchase);
     //empty inputs
     $('#purchaseNameIn').val('');
-    $('#purchasePriceIn').val('');
+    $('#purchasePriceIn').val(''); //.val will always return a STRING
     //calculate remainingBudget
-}
+    calculateRemainingBudget();
+    //update DOM
+    displayPurchases();
+} //end addPurchase
+
+function calculateRemainingBudget(){
+    console.log('inCalculateRem....');
+    // loop through purchases array
+    let totalPrices = 0;
+    for ( let i = 0; i < purchases.length; i++){
+    //for each purchase, add up the total of all prices
+    totalPrices += Number(purchases [ i ].price);
+    } //end for loop
+    console.log( 'totalPrices:', totalPrices);
+    //subtract totalPrices from the budget for remainingBudget
+    const remainingBudget = budget - totalPrices;
+    //display remainingBudget
+    let el = $('#remainingBudgetOut');
+    el.empty();
+    el.append( remainingBudget );
+} //end calculateRemainingBudget
+
+function displayPurchases(){
+    console.log(displayPurchases)
+    //target output by ID
+    let el = $('#purchasesOut');
+    //empty
+    el.empty();
+    //loop through purchases array
+    for (purchase of purchases) {
+    //for each purchase, create a list item
+    el.append( '<li>' + purchase.name + ': $' + purchase.price + '</li>' );
+    } //end for loop
+} //end displayPurchases
 
 function readyNow() {
     //display budget
@@ -28,4 +61,6 @@ el.empty();
 el.append( budget );
 //handle click event
 $('#addPurchaseButton').on('click', addPurchase);
+//init display remainingBudget
+calculateRemainingBudget();
 } //end readyNow
